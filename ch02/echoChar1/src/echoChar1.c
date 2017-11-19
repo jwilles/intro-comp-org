76963f320ed0fe5ec98f1389e9f5683b9f669dc0
Jwilles <johnwilles@gmail.com>
2017-11-19 22:01:26 +0000

    Finish p2

--- ch02/echoChar1/src/echoChar1.c
+++ ch02/echoChar1/src/echoChar1.c
@@ -1,13 +1,33 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(void)
 {
-  char aLetter;
+  char aString[200];
+  char *strPtr = aString;
 
-  write(STDOUT_FILENO, "Enter one character: ", 21);
-  read(STDIN_FILENO, &aLetter, 1);
-  write(STDOUT_FILENO, "You entered: ", 13);
-  write(STDOUT_FILENO, &aLetter, 1);
+  write(STDOUT_FILENO, "Enter a string: ", 16);
+ 
+  read(STDIN_FILENO, strPtr, 1);
+  while (*strPtr != '\n')
+  {
+    strPtr++;
+    read(STDIN_FILENO, strPtr, 1);
+  }
+  *strPtr = '\0';
+
+
+  printf("You entered:\n%s\n", aString);
+
+//  write(STDOUT_FILENO, "You entered:\n", 14);
+//  strPtr = aString;
+//  do
+//  {
+//    write(STDOUT_FILENO, strPtr, 1);
+//    strPtr++;
+//  } while (*strPtr != '\n');
+//  write(STDOUT_FILENO, strPtr, 1);
 
   return 0;
 }
